337025a7d56749df29cbd1ef0d4a5e51beb4d87b
Christopher Cordero <ccordero@protonmail.com>
2019-11-09 15:31:57 -0500

    Re-completed problem 24

--- leetcode/0024_swap_nodes_in_pairs.py
+++ leetcode/0024_swap_nodes_in_pairs.py
@@ -1,34 +1,19 @@
-from typing import Any, Optional
+from typing import Optional
 
 
 class ListNode:
-    def __init__(self, x: Any) -> None:
-        self.val = x
-        self.next: Optional["ListNode"] = None
+    # Provided by LeetCode
+    ...
 
 
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
-
-        new_head = head.next
-
-        if not new_head:
+        if not head or not head.next:
             return head
 
-        current_node = head
-        i = 0
-        while current_node and current_node.next and current_node.next.next:
-            if i % 2 != 0:
-                temp = current_node.next
-                current_node.next = current_node.next.next
-                temp.next = current_node.next.next
-                current_node.next.next = temp
-            current_node = current_node.next
-            i += 1
+        new_head = head.next
+        next_pair = new_head.next
 
-        if head.next:
-            head.next = head.next.next
-            new_head.next = head
+        new_head.next = head
+        head.next = self.swapPairs(next_pair)
         return new_head
