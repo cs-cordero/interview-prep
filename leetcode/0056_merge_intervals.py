d86743a71d42816c87a849c95e16543641016166
Christopher Cordero <ccordero@protonmail.com>
2019-11-14 09:17:59 -0500

    Re-completed problem 56

--- leetcode/0056_merge_intervals.py
+++ leetcode/0056_merge_intervals.py
@@ -3,22 +3,28 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        results = []
         if not intervals:
-            return results
+            return []
 
-        intervals = sorted(intervals)
-        lo, hi = intervals[0]
-        for next_lo, next_hi in intervals[1:]:
-            if next_lo <= hi:
-                hi = max(hi, next_hi)
+        result = []
+        sorted_intervals = sorted(intervals)
+        low, high = sorted_intervals[0]
+        for interval_low, interval_high in sorted_intervals[1:]:
+            if (
+                (interval_low <= low and interval_high >= low and interval_high <= high)
+                or (
+                    interval_low >= low
+                    and interval_low <= high
+                    and interval_high >= high
+                )
+                or (interval_low <= low and interval_high >= high)
+                or (interval_low >= low and interval_high <= high)
+            ):
+                high = max(high, interval_high)
+                low = min(low, interval_low)
             else:
-                results.append([lo, hi])
-                lo, hi = next_lo, next_hi
-
-        results.append([lo, hi])
-        return results
-
-
-foo = Solution().merge([[1, 3], [2, 6], [8, 10], [15, 18]])
-assert foo == [[1, 6], [8, 10], [15, 18]]
+                result.append([low, high])
+                low = interval_low
+                high = interval_high
+        result.append([low, high])
+        return result
