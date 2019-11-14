90bde5d9990359d39acc8f549ab42b3a47e77a9c
Christopher Cordero <ccordero@protonmail.com>
2019-11-13 21:24:32 -0500

    Re-completed problem 22

--- leetcode/0022_generate_parentheses.py
+++ leetcode/0022_generate_parentheses.py
@@ -3,14 +3,20 @@ from typing import List
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        return self._recurse("", n, n)
+        result = []
 
-    def _recurse(self, current_str: str, opens: int, closes: int) -> List[str]:
-        results: List[str] = []
-        if opens > 0:
-            results.extend(self._recurse(current_str + "(", opens - 1, closes))
-        if closes > opens:
-            results.extend(self._recurse(current_str + ")", opens, closes - 1))
-        if opens == 0 and closes == 0:
-            results.append(current_str)
-        return results
+        def helper(current: str, opens: int, closes: int) -> None:
+            if opens == n and closes == n:
+                result.append(current)
+                return
+
+            if opens < n:
+                helper(f"{current}(", opens + 1, closes)
+            if closes < opens:
+                helper(f"{current})", opens, closes + 1)
+
+        helper("", 0, 0)
+        return result
+
+
+print(Solution().generateParenthesis(3))
