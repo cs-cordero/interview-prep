b776919390df3d96b923a843e1a18a45e0f192e8
Christopher Cordero <ccordero@protonmail.com>
2019-11-13 21:42:16 -0500

    Completed problem 34

--- leetcode/0034_find_first_and_last.py
+++ leetcode/0034_find_first_and_last.py
@@ -1,36 +1,10 @@
-from typing import List, Tuple
+from bisect import bisect, bisect_left
+from typing import List
 
 
-def solution(arr: List[int], num: int) -> Tuple[int, int]:
-    first = bisect(arr, num, True)
-    last = bisect(arr, num, False)
-    return (first, last)
-
-
-def bisect(arr: List[int], target: int, first: bool) -> int:
-    left = 0
-    right = len(arr) - 1
-
-    while left <= right:
-        mid = (right + left) // 2
-        if arr[mid] > target:
-            right = mid - 1
-            continue
-        elif arr[mid] < target:
-            left = mid + 1
-            continue
-
-        if first:
-            if mid == 0 or arr[mid - 1] < arr[mid]:
-                return mid
-            right = mid - 1
-
-        else:
-            if mid == len(arr) - 1 or arr[mid + 1] > arr[mid]:
-                return mid
-            left = mid + 1
-    return -1
-
-
-print(solution([5, 7, 7, 8, 8, 10], 8))
-print(solution([5, 7, 7, 8, 8, 10], 6))
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        result = [bisect_left(nums, target), max(bisect(nums, target) - 1, 0)]
+        return [
+            val if val < len(nums) and nums[val] == target else -1 for val in result
+        ]
