bfbfa24d11d1614cabf6fd9f9c42d6a394aa4e4f
Christopher Cordero <ccordero@protonmail.com>
2019-11-14 19:42:48 -0500

    Re-completed problem 5

--- leetcode/0005_longest_palindromic_substring.py
+++ leetcode/0005_longest_palindromic_substring.py
@@ -1,39 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) <= 1:
-            return s
+        if not s:
+            return ""
 
-        longest_palindrome = ""
+        def find_palindrome(left: int, right: int) -> str:
+            if s[left] != s[right]:
+                return ""
 
-        for i in range(len(s) - 1):
-            double_palindrome = (
-                self.get_palindrome(s, i, i + 1) if s[i] == s[i + 1] else ""
-            )
-            single_palindrome = self.get_palindrome(s, i, i)
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
 
-            current_palindrome = (
-                double_palindrome
-                if len(double_palindrome) > len(single_palindrome)
-                else single_palindrome
-            )
+            return s[left + 1 : right]
 
-            if len(current_palindrome) > len(longest_palindrome):
-                longest_palindrome = current_palindrome
-        return longest_palindrome
-
-    def get_palindrome(self, s: str, left: int, right: int) -> str:
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-        return s[left] if left == right else s[left + 1 : right]
-
-
-assert Solution().longestPalindrome("") == ""
-assert Solution().longestPalindrome("a") == "a"
-assert Solution().longestPalindrome("ab") == "a"
-assert Solution().longestPalindrome("aa") == "aa"
-assert Solution().longestPalindrome("abcba") == "abcba"
-assert Solution().longestPalindrome("bbbb") == "bbbb"
-assert Solution().longestPalindrome("bbbbbb") == "bbbbbb"
-assert Solution().longestPalindrome("gfabcbafh") == "fabcbaf"
-assert Solution().longestPalindrome("gfabcbafh") == "fabcbaf"
+        best_palindrome = s[0]
+        for i, value in enumerate(s[1:], 1):
+            for args in [(i, i), (i - 1, i)]:
+                palindrome = find_palindrome(*args)
+                if len(palindrome) > len(best_palindrome):
+                    best_palindrome = palindrome
+        return best_palindrome
