0ee09234b6356a9134c4ced58a41645fee0a7c12
Christopher Cordero <ccordero@protonmail.com>
2019-11-17 19:15:24 -0500

    Re-completed problem 72

--- leetcode/0072_edit_distance.py
+++ leetcode/0072_edit_distance.py
@@ -1,33 +1,15 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp = [[None for _ in range(len(word2))] for _ in range(len(word1))]
+        dp = [[None for _ in range(len(word2) + 1)] for _ in range(len(word1) + 1)]
+        for i in range(len(word1) + 1):
+            dp[i][0] = i
+        for i in range(len(word2) + 1):
+            dp[0][i] = i
 
-        def match(i: int, j: int) -> int:
-            if i >= len(word1):
-                return len(word2) - j
-            elif j >= len(word2):
-                return len(word1) - i
-            elif dp[i][j] is not None:
-                return dp[i][j]
-
-            if word1[i] == word2[j]:
-                dp[i][j] = match(i + 1, j + 1)
-            else:
-                dp[i][j] = (
-                    min(match(i + 1, j + 1), match(i + 1, j), match(i, j + 1)) + 1
-                )
-            return dp[i][j]
-
-        return match(0, 0)
-
-
-assert Solution().minDistance("horse", "ros") == 3
-assert Solution().minDistance("hello", "world") == 4
-assert Solution().minDistance("intention", "execution") == 5
-assert Solution().minDistance("prosperity", "properties") == 4
-assert Solution().minDistance("dinitrophenylhydrazine", "acetylphenylhydrazine") == 6
-assert (
-    Solution().minDistance("trinitrophenylmethylnitramine", "dinitrophenylhydrazine")
-    == 10
-)
-assert Solution().minDistance("zoologicoarchaeologist", "zoogeologist") == 10
+        for i, word1_character in enumerate(word1, 1):
+            for j, word2_character in enumerate(word2, 1):
+                if word1_character != word2_character:
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+                else:
+                    dp[i][j] = dp[i - 1][j - 1]
+        return dp[-1][-1]
