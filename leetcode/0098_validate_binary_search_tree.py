4567524288a278c8b07dcfe00eaaac1db5886eff
Christopher Cordero <ccordero@protonmail.com>
2019-11-22 13:59:21 -0500

    Re-completed problem 98

--- leetcode/0098_validate_binary_search_tree.py
+++ leetcode/0098_validate_binary_search_tree.py
@@ -1,38 +1,21 @@
+from typing import Optional
+
+
 class TreeNode:
-    # Provided by leetcode
+    # Provided by Leetcode
     ...
 
 
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
-        def helper(node: TreeNode):
-            if node.left:
-                left_valid, left_min, left_max = helper(node.left)
-            else:
-                left_valid = True
-                left_min = left_max = None
-            if node.right:
-                right_valid, right_min, right_max = helper(node.right)
-            else:
-                right_valid = True
-                right_min = right_max = None
+        def helper(
+            node: Optional[TreeNode], lower=float("-inf"), upper=float("inf")
+        ) -> bool:
+            if node is None:
+                return True
 
-            valid = (
-                left_valid
-                and right_valid
-                and (left_max is None or node.val > left_max)
-                and (right_min is None or node.val < right_min)
-            )
-            next_min = min(
-                left_min if left_min else node.val,
-                right_min if right_min else node.val,
-                node.val,
-            )
-            next_max = max(
-                left_max if left_max else node.val,
-                right_max if right_max else node.val,
-                node.val,
-            )
-            return valid, next_min, next_max
+            left = helper(node.left, lower=lower, upper=node.val)
+            right = helper(node.right, lower=node.val, upper=upper)
+            return left and right and lower < node.val and node.val < upper
 
-        return helper(root)[0] if root else True
+        return helper(root)
