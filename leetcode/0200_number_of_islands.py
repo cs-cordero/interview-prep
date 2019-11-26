10b3d5825bf0d9f6968599a984c943fe774454a0
Christopher Cordero <ccordero@protonmail.com>
2019-11-26 12:17:40 -0500

    Re-completed problem 200

--- leetcode/0200_number_of_islands.py
+++ leetcode/0200_number_of_islands.py
@@ -1,40 +1,36 @@
 from collections import deque
 from typing import Iterable, List, Tuple
 
-Point = Tuple[int, int]
+
+def get_adjacent(row: int, col: int, M: int, N: int) -> Iterable[Tuple[int, int]]:
+    if row - 1 >= 0:
+        yield row - 1, col
+    if col - 1 >= 0:
+        yield row, col - 1
+    if row + 1 < M:
+        yield row + 1, col
+    if col + 1 < N:
+        yield row, col + 1
 
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        queue = deque()
         islands = 0
         if not grid or not grid[0]:
             return islands
 
-        limits = len(grid), len(grid[0])
-        for row_index, row in enumerate(grid):
-            for col_index, value in enumerate(row):
-                if value == "0":
-                    continue
-                queue = deque([(row_index, col_index)])
+        M, N = len(grid), len(grid[0])
+        for row_i, row in enumerate(grid):
+            for col_i, value in enumerate(row):
+                if value == "1":
+                    islands += 1
+                    queue.append((row_i, col_i))
+                    grid[row_i][col_i] = "0"
                 while queue:
-                    x1, y1 = queue.popleft()
-                    grid[x1][y1] = "0"
-                    for x2, y2 in get_adjacent_points((x1, y1), limits):
-                        if grid[x2][y2] == "1":
-                            grid[x2][y2] = "0"
-                            queue.append((x2, y2))
-                islands += 1
-
+                    current_row, current_col = queue.popleft()
+                    for nrow, ncol in get_adjacent(current_row, current_col, M, N):
+                        if grid[nrow][ncol] == "1":
+                            grid[nrow][ncol] = "0"
+                            queue.append((nrow, ncol))
         return islands
-
-
-def get_adjacent_points(point: Point, limits: Tuple[int, int]) -> Iterable[Point]:
-    x, y = point
-    if x - 1 >= 0:
-        yield x - 1, y
-    if y - 1 >= 0:
-        yield x, y - 1
-    if x + 1 < limits[0]:
-        yield x + 1, y
-    if y + 1 < limits[1]:
-        yield x, y + 1
