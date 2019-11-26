a8ee3e7b55b0a74d2fe3a04db451454c8249cca9
Christopher Cordero <ccordero@protonmail.com>
2019-11-26 12:18:16 -0500

    Re-completed problem 1007

--- leetcode/1007_minimum_domino_rotations_for_equal_row.py
+++ leetcode/1007_minimum_domino_rotations_for_equal_row.py
@@ -1,30 +1,22 @@
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def minDominoRotations(self, A: List[int], B: List[int]) -> int:
-        if not A or not B or len(A) != len(B):
-            return -1
-        if len(A) == 1:
-            return 0
+        countsA = defaultdict(int)
+        countsB = defaultdict(int)
 
-        first_domino, *dominos = [set(domino) for domino in zip(A, B)]
-        common_values = first_domino.intersection(*dominos)
-        if not common_values:
-            return -1
+        possible = None
+        for a, b in zip(A, B):
+            if possible is None:
+                possible = {a, b}
+            else:
+                possible &= {a, b}
+            if not possible:
+                return -1
+            countsA[a] += a != b
+            countsB[b] += a != b
 
-        min_rotations = max(len(A), len(B))
-        for row in [A, B]:
-            for common_value in common_values:
-                count = 0
-                for i in range(len(row)):
-                    if row[i] == common_value and (
-                        (A[i] == common_value) != (B[i] == common_value)
-                    ):
-                        count += 1
-                min_rotations = min(min_rotations, count)
-        return min_rotations
-
-
-print(Solution().minDominoRotations(A=[2, 1, 2, 4, 2, 2], B=[5, 2, 6, 2, 3, 2]))
-print(Solution().minDominoRotations(A=[3, 5, 1, 2, 3], B=[3, 6, 3, 3, 4]))
+        value = next(iter(possible))
+        return min(countsA[value], countsB[value])
