ec94c47d5ae4b6d29aa60127e337c97e89213a8f
Christopher Cordero <ccordero@protonmail.com>
2019-12-03 21:56:53 -0500

    Re-completed problem 11

--- leetcode/0011_container_with_most_water.py
+++ leetcode/0011_container_with_most_water.py
@@ -3,9 +3,11 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        if len(height) < 2:
+            return 0
+
         left = 0
         right = len(height) - 1
-
         best = 0
         while left < right:
             best = max(best, min(height[left], height[right]) * (right - left))
@@ -14,3 +16,6 @@ class Solution:
             else:
                 right -= 1
         return best
+
+
+print(Solution().maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
