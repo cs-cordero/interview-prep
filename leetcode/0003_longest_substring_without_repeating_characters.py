bfdd4641bc86f3cfb01446353202e0c49b99d38f
Christopher Cordero <ccordero@protonmail.com>
2019-12-03 21:56:42 -0500

    Re-completed problem 3

--- leetcode/0003_longest_substring_without_repeating_characters.py
+++ leetcode/0003_longest_substring_without_repeating_characters.py
@@ -1,37 +1,13 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) <= 1:
-            return len(s)
+        seen = set()
+        slow = 0
+        result = 0
+        for fast, character in enumerate(s):
+            while character in seen:
+                seen.remove(s[slow])
+                slow += 1
 
-        mid = len(s) // 2
-        left = self.lengthOfLongestSubstring(s[:mid])
-        right = self.lengthOfLongestSubstring(s[mid:])
-
-        seen = {s[mid]}
-        cross = 1
-        # expand right
-        i = mid + 1
-        while i < len(s) and s[i] not in seen:
-            seen.add(s[i])
-            i += 1
-            cross = max(cross, len(seen))
-        i -= 1
-
-        # expand left with sliding window
-        j = mid - 1
-        while j >= 0:
-            while s[j] in seen and i > mid and j >= 0:
-                seen.remove(s[i])
-                i -= 1
-            if s[j] in seen:
-                break
-            seen.add(s[j])
-            cross = max(cross, len(seen))
-            j -= 1
-
-        return max(cross, left, right)
-
-
-print(Solution().lengthOfLongestSubstring("abcabcbb"))
-print(Solution().lengthOfLongestSubstring("bbbbb"))
-print(Solution().lengthOfLongestSubstring("pwwkew"))
+            seen.add(character)
+            result = max(result, fast - slow + 1)
+        return result
