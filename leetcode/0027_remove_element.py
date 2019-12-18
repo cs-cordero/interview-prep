18ab398a3d07938b70561992fde203a8f51382c3
Christopher Cordero <ccordero@protonmail.com>
2019-12-18 10:09:44 -0500

    Re-completed problem 27

--- leetcode/0027_remove_element.py
+++ leetcode/0027_remove_element.py
@@ -3,20 +3,14 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        swap_point = len(nums) - 1
+        write = len(nums) - 1
+        read = 0
+        if not nums:
+            return 0
 
-        def move_swap_point() -> None:
-            nonlocal swap_point
-
-            while swap_point >= 0 and nums[swap_point] == val:
-                nums[swap_point] = None
-                swap_point -= 1
-
-        i = 0
-        move_swap_point()
-        while i < swap_point:
-            if nums[i] == val:
-                nums[i], nums[swap_point] = nums[swap_point], nums[i]
-                move_swap_point()
-            i += 1
-        return swap_point + 1
+        while read <= write:
+            while read <= write and nums[read] == val:
+                nums[read], nums[write] = nums[write], nums[read]
+                write -= 1
+            read += 1
+        return max(write + 1, 0)
