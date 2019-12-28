280d0bccfe4a8c5b4dfb051fee69b4314c2b91a9
Christopher Cordero <ccordero@protonmail.com>
2019-12-28 10:26:37 -0500

    Completed problem 30

---
+++ leetcode/0030_substring_with_concatenation_of_all_words.py
@@ -0,0 +1,44 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        result = []
+
+        if not s or not words:
+            return result
+
+        reference_counts = Counter(words)
+        word_len = len(words[0])
+
+        for start in range(word_len):
+            begin = start
+            counts = Counter()
+            required = len(reference_counts)
+            for end in range(begin, len(s), word_len):
+                current_word = s[end : end + word_len]
+                if current_word not in reference_counts:
+                    required = len(reference_counts)
+                    begin = end + word_len
+                    counts.clear()
+                    continue
+
+                while counts[current_word] == reference_counts[current_word]:
+                    removed_word = s[begin : begin + word_len]
+                    if counts[removed_word] == reference_counts[removed_word]:
+                        required += 1
+                    counts[removed_word] -= 1
+                    begin += word_len
+
+                counts[current_word] += 1
+                if counts[current_word] == reference_counts[current_word]:
+                    required -= 1
+
+                if not required:
+                    result.append(begin)
+        return result
+
+
+assert Solution().findSubstring("barfoothefoobarman", ["foo", "bar"]) == [0, 9]
+assert Solution().findSubstring("aaaaa", ["aa", "aa"]) == [0, 1]
