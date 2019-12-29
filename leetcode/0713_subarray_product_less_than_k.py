9346fb1a1317b440fa1d83b3818ac53db5b06478
Christopher Cordero <ccordero@protonmail.com>
2019-12-29 02:24:00 -0500

    Re-completed problem 713

--- leetcode/0713_subarray_product_less_than_k.py
+++ leetcode/0713_subarray_product_less_than_k.py
@@ -4,15 +4,21 @@ from typing import List
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
         result = 0
-        for start in range(len(nums)):
-            current = 1
-            for end in range(start, len(nums)):
-                current *= nums[end]
-                if current < k:
-                    result += 1
-                else:
-                    break
+
+        start = 0
+        current = 1
+        for end, value in enumerate(nums):
+            current *= value
+            while start < end and current >= k:
+                current /= nums[start]
+                start += 1
+
+            if current < k:
+                result += end - start + 1
+
         return result
 
 
-print(Solution().numSubarrayProductLessThanK([10, 2, 2, 5, 4, 4, 4, 3, 7, 7], 289))
+assert (
+    Solution().numSubarrayProductLessThanK([10, 2, 2, 5, 4, 4, 4, 3, 7, 7], 289) == 31
+)
