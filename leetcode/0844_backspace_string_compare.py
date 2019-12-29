8dcccc5866f20a11c1e6cceff9f510ac61a47860
Christopher Cordero <ccordero@protonmail.com>
2019-12-29 02:24:12 -0500

    Re-completed problem 844

--- leetcode/0844_backspace_string_compare.py
+++ leetcode/0844_backspace_string_compare.py
@@ -1,18 +1,31 @@
-from typing import List
-
-
 class Solution:
     def backspaceCompare(self, S: str, T: str) -> bool:
-        def parse_string(characters: str) -> List[str]:
-            s = []
-            for character in characters:
-                if character != "#":
-                    s.append(character)
-                elif s:
-                    s.pop()
-            return s
-
-        return parse_string(S) == parse_string(T)
+        s_index = len(S) - 1
+        t_index = len(T) - 1
+        while True:
+            if s_index < 0 and t_index < 0:
+                return True
+            elif S[s_index] == "#":
+                backspace_count = 1
+                s_index -= 1
+                while s_index >= 0 and backspace_count > 0:
+                    backspace_count += 1 if S[s_index] == "#" else -1
+                    s_index -= 1
+            elif T[t_index] == "#":
+                backspace_count = 1
+                t_index -= 1
+                while t_index >= 0 and backspace_count > 0:
+                    backspace_count += 1 if T[t_index] == "#" else -1
+                    t_index -= 1
+            elif s_index >= 0 and t_index >= 0:
+                if S[s_index] != T[t_index]:
+                    return False
+                s_index -= 1
+                t_index -= 1
+            elif s_index < 0 or t_index < 0:
+                return False
 
 
 Solution().backspaceCompare("y#fo##f", "y#f#o##f")
+Solution().backspaceCompare("ab##", "c#d#")
+Solution().backspaceCompare("bxj##tw", "bxj###tw")
