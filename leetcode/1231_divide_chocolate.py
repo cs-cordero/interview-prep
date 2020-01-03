9abb4ca8724087a1d1f7e6da33e60aca80a10f35
Christopher Cordero <ccordero@protonmail.com>
2020-01-03 17:06:27 -0500

    Completed problem 1231

--- leetcode/1231_divide_chocolate.py
+++ leetcode/1231_divide_chocolate.py
@@ -1,32 +1,33 @@
-from typing import List
+from typing import List, Optional
 
 
 class Solution:
     def maximizeSweetness(self, sweetness: List[int], K: int) -> int:
-        def helper(target: int) -> int:
-            groups = 1
-            running_total = 0
+        def helper(k: int, minimum: int) -> Optional[int]:
+            current = 0
+            least_sweet = float("inf")
             for sweet in sweetness:
-                running_total += sweet
-                if running_total >= target:
-                    groups += 1
-                    running_total = 0
-            return groups
+                current += sweet
+                if current >= minimum:
+                    least_sweet = min(least_sweet, current)
+                    current = 0
+                    k -= 1
+                    if k == 0:
+                        break
+            return least_sweet if k == 0 else None
 
-        left = 0
-        right = sum(sweetness)
+        left, right = min(sweetness), sum(sweetness)
+        answer = 0
         while left <= right:
             mid = (left + right) // 2
-            groups = helper(mid)
-            if groups > K + 1:
-                left = mid + 1
-            else:
+            least_sweet = helper(K + 1, mid)
+            if least_sweet is None:
+                # could not generate enough groups
+                # minimum is too high
                 right = mid - 1
-        return right
-
-
-print(
-    Solution().maximizeSweetness(
-        [90670, 55382, 95298, 95795, 73204, 41464, 18675, 30104, 47442, 55307], 6
-    )
-)
+            else:
+                # mid was a small enough minimum to
+                # generate at least enough groups.
+                answer = max(answer, least_sweet)
+                left = mid + 1
+        return answer
