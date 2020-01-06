882aafa71b81d289df9c05935d925fb65aed6b01
Christopher Cordero <ccordero@protonmail.com>
2020-01-05 23:57:32 -0500

    Re-completed problem 33

--- leetcode/0033_search_rotated_sorted_array.py
+++ leetcode/0033_search_rotated_sorted_array.py
@@ -3,23 +3,21 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if not nums:
-            return -1
-        left, right = 0, len(nums) - 1
-
+        left = 0
+        right = len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
 
-            left_side_is_sorted = nums[left] <= nums[mid]
-            if left_side_is_sorted:
-                if target >= nums[left] and target < nums[mid]:
+            left_hand_is_sorted = nums[left] <= nums[mid]
+            if left_hand_is_sorted:
+                if nums[left] <= target and target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
             else:
-                if target <= nums[right] and target > nums[mid]:
+                if nums[mid] < target and target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
