3042835619a9b58663ec8d6b505743ed06359db0
Christopher Cordero <ccordero@protonmail.com>
2020-01-05 21:42:19 -0500

    Re-completed problem 122

--- leetcode/0122_best_time_to_buy_and_sell_stock_ii.py
+++ leetcode/0122_best_time_to_buy_and_sell_stock_ii.py
@@ -3,20 +3,13 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) <= 1:
+        if not prices:
             return 0
 
-        max_profit = 0
-        buy_price = prices[0]
-        current_profit = 0
-        for i in range(1, len(prices)):
-            price = prices[i]
-            if price < prices[i - 1]:
-                max_profit += current_profit
-                current_profit = 0
-                buy_price = price
-            else:
-                current_profit = max(current_profit, price - buy_price)
-        if current_profit > 0:
-            max_profit += current_profit
-        return max_profit
+        profit = 0
+        prev = prices[0]
+        for price in prices:
+            if price > prev:
+                profit += price - prev
+            prev = price
+        return profit
