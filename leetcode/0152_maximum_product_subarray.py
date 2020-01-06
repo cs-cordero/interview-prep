188ff9713c46a20241fe305c97aefc350e029a52
Christopher Cordero <ccordero@protonmail.com>
2020-01-05 23:57:48 -0500

    Re-completed problem 152

--- leetcode/0152_maximum_product_subarray.py
+++ leetcode/0152_maximum_product_subarray.py
@@ -6,30 +6,13 @@ class Solution:
         if not nums:
             return 0
 
-        dp = [1 for _ in range(len(nums))]
-        negative_count = 0
-        for i in range(len(nums) - 1, -1, -1):
-            dp[i] = negative_count
+        prev_max, current_max = nums[0]
+        prev_min, current_min = nums[0]
+        global_max = nums[0]
 
-            value = nums[i]
-            if value == 0:
-                negative_count = 0
-            elif value < 0:
-                negative_count += 1
-
-        def helper(start: int, allow_recurse: bool = True) -> int:
-            current = 1
-            best = float("-inf")
-            for i, num in enumerate(nums[start:], start):
-                current *= num
-                best = max(best, current)
-                if allow_recurse and current < 0 and dp[i] % 2 == 0:
-                    best = max(best, helper(i + 1, False))
-                if current == 0 or (current < 0 and dp[i] == 0):
-                    current = 1
-            return best
-
-        return helper(0)
-
-
-print(Solution().maxProduct([2, -5, -2, -4, 3]))
+        for i in range(1, len(nums)):
+            current_max = max(nums[i], prev_max * nums[i], prev_min * nums[i])
+            current_min = min(nums[i], prev_max * nums[i], prev_min * nums[i])
+            prev_min, prev_max = current_min, current_max
+            global_max = max(global_max, current_max, current_min)
+        return global_max
