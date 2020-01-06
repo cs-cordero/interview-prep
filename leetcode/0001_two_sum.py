a7ff9444b326acf0e8fb7e0e9482cd7f8ae135a2
Christopher Cordero <ccordero@protonmail.com>
2020-01-05 21:41:53 -0500

    Re-completed problem 1

--- leetcode/0001_two_sum.py
+++ leetcode/0001_two_sum.py
@@ -3,9 +3,10 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapping = {}
+        hashmap = {}
         for i, num in enumerate(nums):
-            if target - num in mapping:
-                return [mapping[target - num], i]
-            mapping[num] = i
+            k = target - num
+            if k in hashmap:
+                return [hashmap[k], i]
+            hashmap[num] = i
         return []
