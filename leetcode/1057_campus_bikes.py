2a7d27d5c007db8bae1aae42546cdca9f99fe8cf
Christopher Cordero <ccordero@protonmail.com>
2020-01-11 10:42:27 -0500

    Re-completed problem 1057

--- leetcode/1057_campus_bikes.py
+++ leetcode/1057_campus_bikes.py
@@ -1,3 +1,4 @@
+from heapq import heappop, heappush
 from typing import List
 
 
@@ -5,21 +6,22 @@ class Solution:
     def assignBikes(
         self, workers: List[List[int]], bikes: List[List[int]]
     ) -> List[int]:
-        sorted_distances = sorted(
-            (abs(by - wy) + abs(bx - wx), wi, bi)
-            for wi, (wx, wy) in enumerate(workers)
-            for bi, (bx, by) in enumerate(bikes)
-        )
+        heap = []
+        for worker_index, worker in enumerate(workers):
+            worker_x, worker_y = worker
+            for bike_index, bike in enumerate(bikes):
+                bike_x, bike_y = bike
+                dist = abs(worker_x - bike_x) + abs(worker_y - bike_y)
 
-        result = [None for _ in range(workers)]
+                key = (dist, worker_index, bike_index)
+                heappush(heap, key)
+
+        result = [None for _ in range(len(workers))]
         assigned_bikes = set()
-        for _, w, b in sorted_distances:
-            if result[w] is not None or b in assigned_bikes:
+        while heap:
+            _, worker_index, bike_index = heappop(heap)
+            if result[worker_index] is not None or bike_index in assigned_bikes:
                 continue
-
-            assigned_bikes.add(b)
-            result[w] = b
-            if len(assigned_bikes) == len(bikes):
-                # all bikes assigned
-                break
+            assigned_bikes.add(bike_index)
+            result[worker_index] = bike_index
         return result
