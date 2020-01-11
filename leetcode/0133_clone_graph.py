be570b98d7e8c3b38d31f7c33d1049b4a7871473
Christopher Cordero <ccordero@protonmail.com>
2020-01-11 10:41:50 -0500

    Re-completed problem 133

--- leetcode/0133_clone_graph.py
+++ leetcode/0133_clone_graph.py
@@ -5,16 +5,14 @@ class Node:
 
 class Solution:
     def cloneGraph(self, node: "Node") -> "Node":
-        clone_map = {}
+        original_to_clone_map = {}
 
-        def helper(node: "Node") -> "Node":
-            if node in clone_map:
-                return clone_map[node]
-
-            clone_map[node] = Node(node.val, neighbors=[])
-            clone_map[node].neighbors = [
-                helper(neighbor) for neighbor in node.neighbors
-            ]
-            return clone_map[node]
+        def helper(original: "Node") -> "Node":
+            if original not in original_to_clone_map:
+                original_to_clone_map[original] = Node(val=original.val, neighbors=[])
+                original_to_clone_map[original].neighbors = [
+                    helper(neighbor) for neighbor in original.neighbors
+                ]
+            return original_to_clone_map[original]
 
         return helper(node)
