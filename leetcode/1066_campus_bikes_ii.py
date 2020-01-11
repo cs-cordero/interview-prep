b599f9a4cf2f06c8e2ec9874b01ca4042ca7deaa
Christopher Cordero <ccordero@protonmail.com>
2020-01-11 10:42:40 -0500

    Re-completed problem 1066

--- leetcode/1066_campus_bikes_ii.py
+++ leetcode/1066_campus_bikes_ii.py
@@ -1,106 +1,90 @@
+from collections import defaultdict
 from dataclasses import dataclass
-from typing import Dict, List, Optional, Tuple
+from heapq import heappop, heappush
+from typing import Dict, List
 
 
 @dataclass
 class Edge:
-    source: int
-    target: int
-    cost: int
-    flow: int = 0
-    capacity: int = 1
-    residual: Optional["Edge"] = None
+    dist: int
+    flow: int
+    max_flow: int
 
     @property
-    def remaining_capacity(self) -> int:
-        return self.capacity - self.flow
-
-    @classmethod
-    def create_pair(
-        cls, source: int, target: int, cost: int, flow: int = 0, capacity: int = 1
-    ) -> Tuple["Edge", "Edge"]:
-        primary = Edge(
-            source=source, target=target, cost=cost, flow=flow, capacity=capacity
-        )
-        residual = Edge(
-            source=target,
-            target=source,
-            cost=-cost,
-            flow=-flow,
-            capacity=0,
-            residual=primary,
-        )
-        primary.residual = residual
-        return primary, residual
+    def capacity(self) -> int:
+        return self.max_flow - self.flow
 
 
 class Solution:
     def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> int:
-        edges = setup_edge_list(workers, bikes)
-        vertices = len(workers) + len(bikes) + 2
-        source_i = vertices - 2
-        target_i = vertices - 1
-        assignments = {}
-        cost = 0
+        graph = create_graph(workers, bikes)
 
-        def is_worker(i: int) -> bool:
-            return i < len(workers)
+        def is_worker(index: str) -> bool:
+            return index not in ("SOURCE", "SINK") and int(index) < len(workers)
 
-        def is_bike(i: int) -> bool:
-            return i < (len(workers) + len(bikes)) and not is_worker(i)
+        def is_bike(index: str) -> bool:
+            return index not in ("SOURCE", "SINK") and int(index) >= len(workers)
 
-        def bellman_ford() -> Tuple[List[int], List[int]]:
-            distances = [float("inf") for _ in range(vertices)]
-            distances[source_i] = 0
-            prev_vertices = [None for _ in range(vertices)]
-            prev_edges = [None for _ in range(vertices)]
-            for _ in range(vertices - 1):
-                for edge_i, edge in enumerate(edges):
-                    if edge.remaining_capacity <= 0:
-                        continue
-                    if distances[edge.target] > distances[edge.source] + edge.cost:
-                        distances[edge.target] = distances[edge.source] + edge.cost
-                        prev_vertices[edge.target] = edge.source
-                        prev_edges[edge.target] = edge_i
-            return prev_vertices, prev_edges
+        assignments = [None for _ in range(len(workers))]
 
-        def update_flow_along_path(
-            reversed_vertex_path: List[int], reversed_edge_path: List[int]
-        ) -> Tuple[Dict[int, int], int]:
-            worker_to_bike_assignments = {}
-            delta_cost = 0
-            vertex = target_i
-            while vertex != source_i:
-                edge = edges[reversed_edge_path[vertex]]
-                edge.flow += 1
-                edge.residual.flow -= 1
-                if is_bike(edge.target) and is_worker(edge.source):
-                    worker_to_bike_assignments[edge.source] = edge.target
-                delta_cost += edge.cost
-                vertex = reversed_vertex_path[vertex]
-            return worker_to_bike_assignments, delta_cost
+        while True:
+            max_flow_path = find_path(graph)
+            if not max_flow_path:
+                break
 
-        while len(assignments) < len(workers):
-            new_assignments, delta_cost = update_flow_along_path(*bellman_ford())
-            assignments.update(new_assignments)
-            cost += delta_cost
-        return cost
+            prev, *path = max_flow_path
+            for next_node in path:
+                graph[prev][next_node].flow += 1
+                graph[next_node][prev].flow -= 1
+                if is_worker(prev) and is_bike(next_node):
+                    assignments[int(prev)] = int(next_node) - len(workers)
+                prev = next_node
+
+        return sum(
+            graph[str(worker)][str(bike + len(workers))].dist
+            for worker, bike in enumerate(assignments)
+        )
 
 
-def setup_edge_list(workers: List[List[int]], bikes: List[List[int]]) -> int:
-    edges = []
-    for worker_i, (worker_x, worker_y) in enumerate(workers):
-        for bike_i, (bike_x, bike_y) in enumerate(bikes):
-            distance = abs(worker_x - bike_x) + abs(worker_y - bike_y)
-            edges.extend(Edge.create_pair(worker_i, bike_i + len(workers), distance))
+def create_graph(
+    workers: List[List[int]], bikes: List[List[int]]
+) -> Dict[str, Dict[str, Edge]]:
+    graph = defaultdict(dict)
+    for worker_index, worker in enumerate(workers):
+        worker_name = str(worker_index)
+        graph["SOURCE"][worker_name] = Edge(dist=0, flow=0, max_flow=1)
+        graph[worker_name]["SOURCE"] = Edge(dist=0, flow=0, max_flow=0)
 
-    source_i = len(workers) + len(bikes)
-    target_i = source_i + 1
-    for worker_i in range(len(workers)):
-        edges.extend(Edge.create_pair(source_i, worker_i, 0))
-    for bike_i in range(len(bikes)):
-        edges.extend(Edge.create_pair(bike_i + len(workers), target_i, 0))
-    return edges
+        for bike_index, bike in enumerate(bikes):
+            wx, wy = worker
+            bx, by = bike
+            dist = abs(wy - by) + abs(wx - bx)
+
+            bike_name = str(bike_index + len(workers))
+            graph[worker_name][bike_name] = Edge(dist=dist, flow=0, max_flow=1)
+            graph[bike_name][worker_name] = Edge(dist=-dist, flow=0, max_flow=0)
+
+            if "SINK" not in graph[bike_name]:
+                graph[bike_name]["SINK"] = Edge(dist=0, flow=0, max_flow=1)
+                graph["SINK"][bike_name] = Edge(dist=0, flow=0, max_flow=0)
+
+    return graph
+
+
+def find_path(graph: Dict[str, Dict[str, Edge]]) -> List[str]:
+    heap = [(0, ["SOURCE"])]
+    while heap:
+        current_dist, current_path = heappop(heap)
+        current_node = current_path[-1]
+        if current_node == "SINK":
+            return current_path
+
+        for neighbor, edge in graph[current_node].items():
+            if edge.capacity == 0 or neighbor in current_path:
+                continue
+            key = (current_dist + edge.dist, current_path + [neighbor])
+            heappush(heap, key)
+    return []
 
 
 print(Solution().assignBikes([[0, 0], [0, 2]], [[0, 1], [0, 7]]))  # 6
